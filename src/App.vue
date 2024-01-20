5f33fdc0e0dbee8e675590041c90fdfaeb24c9da
DavidePeloso-dev <davide.peloso.e@gmail.com>
2024-01-20 13:03:33 +0100

    v-model v-on v-binding

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,44 @@
 <script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: 'Vue',
+      firstName: '',
+      lastName: '',
+      text: 'text-primary'
+    }
+  },
+  methods: {
+    sayHi() {
+      const pEl = document.querySelector('p');
+      const message = `Hi Vue! ~${this.firstName} ${this.lastName}`;
+      console.log(message);
+      pEl.innerHTML = message
+    }
+  }
+}
 </script>
 
 <template>
-  <h1>Hello World!</h1>
+  <h1>Hello from {{ message }}!</h1>
+
+  <div>
+    <div class="mb-3">
+      <label class="form-label" :class="text" for="firstName">First Name:</label>
+      <input class="form-control" id="firstName" type="text" v-model="firstName">
+    </div>
+    <div class="mb-3">
+      <label class="form-label" :class="text" for="lastName">Last Name:</label>
+      <input class="form-control" id="lastName" type="text" v-model="lastName">
+    </div>
+
+    <button class="btn btn-primary mb-3" @click="sayHi">Say Hi to Vue!</button>
+
+    <p></p>
+
+
+  </div>
 </template>
 
 <style></style>
